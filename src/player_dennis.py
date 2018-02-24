b2c7da3be2da8e244c1d803c44cca4833a1ff454
David Gronlund <dagronlund@gmail.com>
2018-02-24 15:57:19 -0500

    Initial algorithm

--- src/player_dennis.py
+++ src/player_dennis.py
@@ -18,6 +18,29 @@ class Player(BasePlayer):
         return
 
     """
+    Returns node of least resistance
+    """
+    def best_node_advantage(self, node):
+        # TODO: If there is a tie, favor the closer one to your center
+        best_advantage = 0
+        best_neighbor = None
+        node_owner = self.board.nodes[node]['owner']
+        node_units = self.board.nodes[node]['old_units']
+        for neighbor in self.board[node]:
+            if node_owner == self.board.nodes[neighbor]['owner']:
+                continue
+
+            neighbor_units = self.board.nodes[neighbor]['old_units']
+            advantage = node_units - neighbor_units
+
+            if best_neighbor is None or advantage > best_advantage:
+                best_advantage = advantage
+                best_neighbor = neighbor
+
+        return best_neighbor, best_advantage
+
+
+    """
     Determines if node is adjacent to an enemy
     """
     def is_threatened_node(self, node):
@@ -90,11 +113,45 @@ class Player(BasePlayer):
         Insert player logic here to determine where to place your units
         """
         
-        for node in self.nodes:
-            self.place_unit(node, self.max_units)
-            break
+        frontier_nodes = []
 
-        print(self.find_player_stats())
+        for node in self.nodes:
+            if self.is_frontier_node(node):
+                frontier_nodes.append(node)
+
+        possible_targets = []
+        for frontier_node in frontier_nodes:
+            node, advantage = self.best_node_advantage(frontier_node)
+            possible_targets.append((frontier_node, node, advantage))
+
+        possible_targets = sorted(possible_targets, key=lambda x: x[2], reverse=True)
+
+        print(possible_targets)
+
+        available_units = self.max_units
+        print('Available')
+        print(available_units)
+        for target in possible_targets:
+            advantage = target[2]
+            if advantage <= 2:
+                required_units = 2 - advantage
+                
+                if available_units > required_units:
+                    # print('Placed ' + str(required_units) + ' at ' + str(target[0]))
+                    self.place_unit(target[0], required_units)
+                    available_units -= required_units
+                else:
+                    # print('Placed ' + available_units + ' at ' + str(target[0]))
+                    self.place_unit(target[0], available_units)
+                    available_units = 0
+
+        print('remaining')
+        print(available_units)
+
+        # Place remaining somewhere
+        for node in self.nodes:
+            self.place_unit(node, available_units)
+            break
 
         return self.dict_moves #Returns moves built up over the phase. Do not modify!
 
@@ -105,5 +162,30 @@ class Player(BasePlayer):
         """
         Insert player logic here to determine where to move your units
         """
+
+        frontier_nodes = []
+
+        for node in self.nodes:
+            if self.is_frontier_node(node):
+                frontier_nodes.append(node)
+
+        possible_targets = []
+        for frontier_node in frontier_nodes:
+            node, advantage = self.best_node_advantage(frontier_node)
+            possible_targets.append((frontier_node, node, advantage))
+
+        for target in possible_targets:
+
+
+            friendly_units = self.board.nodes[target[0]]['old_units']
+            enemy_units = self.board.nodes[target[1]]['old_units']
+
+            # print('Info')
+            # print(target)
+            # print(friendly_units)
+            # print(enemy_units)
+
+            if friendly_units >= enemy_units + 2:
+                self.move_unit(target[0], target[1], enemy_units + 1)            
 
         return self.dict_moves #Returns moves built up over the phase. Do not modify!
